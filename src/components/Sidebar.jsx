bcdfef6703ed11bbca8556ac269576836c43212d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-16 03:12:39 +0530

    Component Functionality:
    Sidebar: This is a functional component that represents the sidebar of your application.
    mainLinks and otherLinks: These are arrays containing objects with icon and name properties. Each object represents a sidebar link, where icon is the icon component and name is the text displayed alongside the icon.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,4 +9,44 @@ import {
 import { LuThumbsUp } from 'react-icons/lu';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
+const Sidebar = () => {
+  const mainLinks = [
+    {
+      icon: <MdHome className="text-3xl text-gray-400" />,
+      name: 'Home',
+    },
+    {
+      icon: <GiHamburgerMenu className="text-3xl text-gray-400" />,
+      name: 'Explore',
+    },
+    {
+      icon: <MdSubscriptions className="text-3xl text-gray-400" />,
+      name: 'Subscriptions',
+    },
+  ];
 
+  const otherLinks = [
+    {
+      icon: <MdVideoLibrary className="text-3xl text-gray-400" />,
+      name: 'Library',
+    },
+    {
+      icon: <MdHistory className="text-3xl text-gray-400" />,
+      name: 'History',
+    },
+    {
+      icon: <MdWatchLater className="text-3xl text-gray-400" />,
+      name: 'Watch Later',
+    },
+    {
+      icon: <LuThumbsUp className="text-3xl text-gray-400" />,
+      name: 'Liked Videos',
+    },
+  ];
+
+  return (
+    
+  );
+};
+
+export default Sidebar;

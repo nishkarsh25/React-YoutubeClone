e0fe72a9c610a00056b58794b73380b7724b69c6
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-16 03:42:31 +0530

    Basic configuration of Redux Toolkit:
    Made general layout of youtube slice and configureStore

---
+++ src/features/youtubeSlice.jsx
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    videos: [],
+    currentPlaying: null,
+    searchTerm:"",
+    searchResults:[],
+    nextPageToken:null,
+    recommendedVideo:[]
+};
+
+
+const youtubeSlice = createSlice({
+    name:"youtubeApp",
+    initialState,
+    reducers:{
+        
+    }
+})
+
+
+export default youtubeSlice.reducer;

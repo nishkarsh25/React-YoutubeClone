1decbdebd1d618b09a27c72c3b33175e2a184e89
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-20 04:41:18 +0530

    createAsyncThunk:
    createAsyncThunk is used to define an asynchronous thunk action creator named getHomePageVideos.
    It takes two arguments:
    The first argument is a string representing the action type prefix. It's used to automatically generate action types for pending, fulfilled, and rejected actions.
    The second argument is an asynchronous function that performs the API request. It receives two parameters: isNext and getState. isNext is a boolean indicating whether to fetch the next page of results, and getState is a function to access the current Redux state.

--- src/store/reducers/getHomePageVideos.jsx
+++ src/store/reducers/getHomePageVideos.jsx
@@ -3,3 +3,9 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_REACT_APP_YOUTUBE_DATA_API_KEY;
 
+export const getHomePageVideos = createAsyncThunk(
+    "youtube/App/searchPageVideos",
+    async(isNext,{getState}) => {
+        
+    }
+)

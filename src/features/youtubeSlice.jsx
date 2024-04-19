dcb5e44beb27248a2e20070aa1e7e07edcc85284
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-20 04:36:48 +0530

    Added extrareducer for getHomePageVideos:

--- src/features/youtubeSlice.jsx
+++ src/features/youtubeSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getHomePageVideos } from "../store/reducers/getHomePageVideos";
 
 const initialState = {
     videos: [],
@@ -14,6 +15,15 @@ const youtubeSlice = createSlice({
     name:"youtubeApp",
     initialState,
     reducers:{
+        
+    },
+    extraReducers:(builder) => {
+        builder.addCase(getHomePageVideos.fulfilled,(state,action)=> {
+            if(action.payload && action.payload.parsedData){
+                state.videos = action.payload.parsedData;
+                state.nextPageToken = action.payload.nextPageToken;
+            }
+        })
         
     }
 })

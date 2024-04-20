fe766729eaf38bdddc922cb3698487470a92c18e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-20 18:31:40 +0530

    Magnitude >= 1 Billion (1.0e9):
    if (num >= 1.0e9) { ... }: Checks if the numerical value is greater than or equal to 1 billion.
    return (num / 1.0e9).toFixed(0) + "B";: If the value is in billions, it divides the value by 1 billion and returns it with a precision of 0 decimal places followed by the letter 'B' to denote billions.

--- src/utils/convertRawtoString.jsx
+++ src/utils/convertRawtoString.jsx
@@ -2,6 +2,8 @@ export const convertRawtoString = (labelValue,isSub=false)=>{
 
     const num = Math.abs(Number(labelValue));
 
-    
+    if(num >=1.0e9){
+        return(num/1.0e9).toFixed(0) +"B"; //1500000000 = 1 B
+    }
     
 }

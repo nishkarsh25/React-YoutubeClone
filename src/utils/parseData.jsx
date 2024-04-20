bbe5b67f9ba8c829140459483f3b6b05eac7109c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-20 16:53:46 +0530

    Environment Variable:
    const API_KEY = import.meta.env.VITE_REACT_APP_YOUTUBE_DATA_API_KEY: Retrieves the YouTube Data API key from environment variables.

--- src/utils/parseData.jsx
+++ src/utils/parseData.jsx
@@ -3,3 +3,5 @@ import { parseVideoDuration } from './parseVideoDuration';
 import { convertRawtoString } from './convertRawtoString';
 import { timeSince } from './timeSince';
 
+const API_KEY = import.meta.env.VITE_REACT_APP_YOUTUBE_DATA_API_KEY;
+

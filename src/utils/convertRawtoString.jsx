213a1a866c47b233bd1906936813ef1d30fe86b1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-20 18:31:55 +0530

    Magnitude >= 1 Million (1.0e6):
    if (num >= 1.0e6) { ... }: Checks if the numerical value is greater than or equal to 1 million.
    return (num / 1.0e6).toFixed(0) + "M";: If the value is in millions, it divides the value by 1 million and returns it with a precision of 0 decimal places followed by the letter 'M' to denote millions.

--- src/utils/convertRawtoString.jsx
+++ src/utils/convertRawtoString.jsx
@@ -5,5 +5,9 @@ export const convertRawtoString = (labelValue,isSub=false)=>{
     if(num >=1.0e9){
         return(num/1.0e9).toFixed(0) +"B"; //1500000000 = 1 B
     }
+    if(num >=1.0e6){
+        return(num/1.0e6).toFixed(0) +"M"; //1500000 = 1M
+        
+    }
     
 }

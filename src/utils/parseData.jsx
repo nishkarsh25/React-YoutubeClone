248a13fefbb1b15545dcd6098530e3564ccdd015
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-20 16:56:15 +0530

    Preparing Data for API Requests:
    const videoIds = []; const channelIds = [];: Initializes arrays to store video IDs and channel IDs.
    items.forEach((item) => { ... });: Loops through the input items and extracts video IDs and channel IDs, pushing them into the respective arrays.

--- src/utils/parseData.jsx
+++ src/utils/parseData.jsx
@@ -8,6 +8,14 @@ const API_KEY = import.meta.env.VITE_REACT_APP_YOUTUBE_DATA_API_KEY;
 export const parseData = async (items) => {
 
   try {
+    const videoIds = [];
+    const channelIds = [];
+
+    items.forEach((item) => {
+      channelIds.push(item.snippet.channelId);
+      videoIds.push(item.id.videoId);
+    });
+
     
   }
   catch (err) {

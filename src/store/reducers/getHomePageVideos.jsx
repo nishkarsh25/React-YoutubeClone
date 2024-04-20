e2f4144d31798e28f4ea5b07c72a9acbf5f6d695
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-20 19:12:26 +0530

    const items = response.data.items;: This line extracts the items array from the response data obtained from the API call. This array typically contains the items (videos in this case) returned by the YouTube API.
    const parsedData = await parseData(items);: This line calls the parseData function to parse the items obtained from the API response. It awaits the completion of this asynchronous operation to get the parsed data.
    
    const nextPageToken = response.data.nextPageToken;: Here, the nextPageToken is extracted from the response data. This token is often provided by APIs to paginate through large datasets. It indicates the token to use in the next request to fetch the next page of results.
    
    dispatch(updateNextPageToken(nextPageToken));: This line dispatches an action (updateNextPageToken) with the nextPageToken as its payload. Dispatching this action will trigger the corresponding reducer to update the nextPageToken in the Redux state.
    
    return { parsedData: [...videos, ...parsedData], nextPageToken };: Finally, this line returns an object containing the parsed data and the nextPageToken. This object will be used as the payload of the fulfilled action dispatched by the createAsyncThunk function. The parsed data will typically be used to update the video list in the Redux state, while the nextPageToken will be stored for future pagination requests.

--- src/store/reducers/getHomePageVideos.jsx
+++ src/store/reducers/getHomePageVideos.jsx
@@ -1,17 +1,29 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { parseData } from "../../utils/parseData";
+import { updateNextPageToken } from "../../features/youtubeSlice";
 
 const API_KEY = import.meta.env.VITE_REACT_APP_YOUTUBE_DATA_API_KEY;
 
 export const getHomePageVideos = createAsyncThunk(
     "youtube/App/searchPageVideos",
-    async(isNext,{getState}) => {
+    async(isNext,{getState, dispatch}) => { // Destructure dispatch from the parameters
         const {
             youtubeApp : {nextPageToken : nextPageTokenFromState,videos},
         } = getState();
         const response = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?maxResults=20&q="drop x out"&key=${API_KEY}&part=snippet&type=video&${
             isNext ? `pageToken=${nextPageTokenFromState}` : ""
           }`);
-        console.log(response)
+        const items = response.data.items;
+        const parsedData = await parseData(items);
+
+        // Extract nextPageToken from the response
+        const nextPageToken = response.data.nextPageToken;
+
+        // Dispatch the updateNextPageToken action to update nextPageToken in the state
+        dispatch(updateNextPageToken(nextPageToken));
+
+        // Return the parsedData and nextPageToken in the payload
+        return { parsedData: [...videos, ...parsedData], nextPageToken };
     }
 )
